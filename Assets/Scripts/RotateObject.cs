a71a2925807ba00449f0e0e0178db04f5d199bd8
Albert Dols Ferrandis <al315151@uji.es>
2018-03-25 20:34:40 +0200

    La detección ahora es correcta
    Gitaneo para ello... pero falta calibrar el movimiento...

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -10,54 +10,32 @@ public class RotateObject : MonoBehaviour {
 
     #region ROTATE_AROUND_VARIABLES
     float distanceFromObjectToCamera;
-    public float rotationForce;
     Vector3 lastFrameMousePosition;
     #endregion
 
     #region MOUSE_DETECTION_VARIABLES
-    Image selfDetectionImage;
-    float timeThresoldInputDetection = 0.05f;
-    float timerInputDetection = 0.0f;
+  
     #endregion
 
 
     // Use this for initialization
     void Start ()
-    {
-        selfDetectionImage = GetComponent<Image>();
+    {       
         if (ShaderEdition.currentInstance != null)
         {
             MeshCamera = ShaderEdition.currentInstance.ActiveCamera;
             MeshReference = ShaderEdition.currentInstance.displayObject;
-        }
-		
+        }		
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (timerInputDetection > timeThresoldInputDetection)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (ClickInsideBounds())
-                { lastFrameMousePosition = Input.mousePosition; }
-            }
-            else if (Input.GetMouseButton(0))
-            {
-                if (ClickInsideBounds())
-                { DetectMousePosition(); }
-            }
-
-            if (MeshCamera == null || MeshReference == null)
-            {
-                MeshCamera = ShaderEdition.currentInstance.ActiveCamera;
-                MeshReference = ShaderEdition.currentInstance.displayObject;
-            }
-
-            timerInputDetection = 0.0f;
-        }
-        timerInputDetection = timerInputDetection + Time.deltaTime;
+       if (MeshCamera == null || MeshReference == null)
+       {
+           MeshCamera = ShaderEdition.currentInstance.ActiveCamera;
+           MeshReference = ShaderEdition.currentInstance.displayObject;
+       }
 	}
 
     void RotateCameraOnMesh(Vector3 axisRotation)
@@ -70,6 +48,7 @@ public class RotateObject : MonoBehaviour {
 
     public void DetectMousePosition()
     {
+        print("We're in!!!");
         /*print(Input.mousePosition + " --> Mouse Position");
         print((selfDetectionImage.transform.position) + " --> Image Position");*/
         Vector3 AxisMovement;
@@ -83,12 +62,12 @@ public class RotateObject : MonoBehaviour {
     }
 
 
-    bool ClickInsideBounds()
-    {
-        return Input.mousePosition.x >= selfDetectionImage.transform.position.x - selfDetectionImage.rectTransform.rect.width / 2 &&
-               Input.mousePosition.x <= selfDetectionImage.transform.position.x + selfDetectionImage.rectTransform.rect.width / 2 &&
-               Input.mousePosition.y >= selfDetectionImage.transform.position.y - selfDetectionImage.rectTransform.rect.height / 2 &&
-               Input.mousePosition.y <= selfDetectionImage.transform.position.y + selfDetectionImage.rectTransform.rect.height / 2;        
-    }
+
+
+
+
+
+
+
 
 }
